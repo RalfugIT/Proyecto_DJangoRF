493c2144bed53c275ace90ee609eccf651c3e66c
RalfugIT <ralfug@gmail.com>
2025-09-24 16:43:17 -0500

    first commit en DJango Rest Framework

---
+++ Directorio_Del_Projecto/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Directorio_Del_Projecto project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Directorio_Del_Projecto.settings')
+
+application = get_asgi_application()
